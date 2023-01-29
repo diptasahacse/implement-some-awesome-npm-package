840fca856463889e6e3e3b903590212902a33b51
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-01-30 02:05:47 +0600

    add github calender

---
+++ pages/github-calender.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import GitHubCalendar from 'react-github-calendar';
+const gitHubCalender = () => {
+  return (
+    <div>
+        <GitHubCalendar username="diptasahacse" />
+    </div>
+  )
+}
+
+export default gitHubCalender
